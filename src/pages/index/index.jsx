dcd653a85f5302a677077190fe987c52741ef114
zilingziling <ziling_ding@qq.com>
2020-05-19 23:25:23 +0800

    首页

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,6 +1,6 @@
 import Taro, { Component } from "@tarojs/taro";
-import { View, Button, Text, Swiper, SwiperItem } from "@tarojs/components";
-import { AtMessage } from "taro-ui";
+import { View, Button, Text,Image,Input } from "@tarojs/components";
+import { AtMessage,AtInput } from "taro-ui";
 import "./index.scss";
 import api from "../../service/api";
 class Index extends Component {
@@ -19,7 +19,52 @@ class Index extends Component {
     return (
       <View className="indexContent">
         <AtMessage />
+         <View className='banner'>
+            <Text className='title'>仓库服务开创者</Text>
+            <Text className='info'>
+              球鞋，服饰，箱包
+              精准调货 快速准确
+            </Text>
+         </View>
+        <View className='counts'>
+          <View className='aData'>
+            <View className='total'>
+              <Text className='bold'>100,00</Text>
+              <Text className='normal'>件</Text>
+            </View>
+            <Text className='explain'>累计交易次数达到</Text>
+          </View>
+          <View className='aData'>
+            <View className='total'>
+              <Text className='bold'>100,00</Text>
+              <Text className='normal'>件</Text>
+            </View>
+            <Text className='explain'>求货总数</Text>
+          </View>
+          <View className='aData'>
+            <View className='total'>
+              <Text className='bold'>100,00</Text>
+              <Text className='normal'>件</Text>
+            </View>
+            <Text className='explain'>出货总数</Text>
+          </View>
 
+        </View>
+        <View className='search'>
+          <Image src={require('../../assets/images/search.png')} className='searchIcon'/>
+          {/*<Input placeholderClass='placeHolder' clear={true} placeholder='输入货号或名字'/>*/}
+          <AtInput
+            className='input'
+            clear
+            placeholderClass='placeHolder'
+            border={false}
+            placeholder='输入货号或名字'
+          />
+        </View>
+        <View className='type'>
+          <View className='btn clicked'>求货</View>
+          <View className='btn'>出货</View>
+        </View>
       </View>
     );
   }
